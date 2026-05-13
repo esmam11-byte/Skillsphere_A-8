5280409d154919d3c87977c7aaf05def2be570f4
esmam11-byte <yeaseresmam2511@gmail.com>
2026-05-13 22:14:09 +0600

    loader added

---
+++ components/Loader.jsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <div className="flex justify-center items-center py-20">
+      <div className="loading loading-spinner loading-lg text-primary"></div>
+    </div>
+  );
+}
